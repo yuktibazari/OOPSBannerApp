ef6b06692d640bcbb5222d465aa3b0f51d980466
yuktibazari <yuktibazari@gmail.com>
2026-02-23 08:47:53 +0530

    Added UC3 Banner App using String Join

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,26 @@
+public class OOPSBannerUC2 {
+
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println("  *****  " + "  *****  " + " ******* " + " ******* ");
+
+        // Line 2
+        System.out.println(" *     * " + " *     * " + " *      *" + " *       ");
+
+        // Line 3
+        System.out.println(" *     * " + " *     * " + " *      *" + " *       ");
+
+        // Line 4
+        System.out.println(" *     * " + " *     * " + " ******* " + "  *****  ");
+
+        // Line 5
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+
+        // Line 6
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+
+        // Line 7
+        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
+    }
+}
